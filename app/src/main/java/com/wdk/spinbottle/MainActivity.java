b7d9ca4ef5a472c67994c6d1c5c84c64ee2fccc6
Wicker <wdkaramatskos@gmail.com>
2021-02-06 15:16:54 +0200

    First Attempt

---
+++ app/src/main/java/com/wdk/spinbottle/MainActivity.java
@@ -0,0 +1,72 @@
+package com.wdk.spinbottle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+   private ImageView bottle;
+   private Random random = new Random();
+   private int Lastdir;
+   private boolean spinning;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        RelativeLayout relativeLayout = findViewById(R.id.layout);
+        AnimationDrawable animationDrawable = (AnimationDrawable) relativeLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(1000);
+        animationDrawable.setExitFadeDuration(2000);
+        animationDrawable.start();
+
+
+
+
+
+
+
+
+        bottle =(ImageView)findViewById(R.id.bottle);
+    }
+
+    public void spinBottle(View v){
+        if (!spinning) {
+            int newDir = random.nextInt(1800);
+            float pivotX = bottle.getWidth() / 2;
+            float pivotY = bottle.getHeight() / 2;
+
+            Animation rotate = new RotateAnimation(Lastdir, newDir, pivotX, pivotY);
+            rotate.setDuration(2500);
+            rotate.setFillAfter(true);
+            rotate.setAnimationListener(new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+                    spinning = true;
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    spinning = false;
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+
+                }
+            });
+
+            Lastdir = newDir;
+            bottle.startAnimation(rotate);
+        }
+    }
+}
